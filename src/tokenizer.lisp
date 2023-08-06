147877165e13f92397e075fc4b44ad0b1c1c70f7
swapn <swapneil.singh@gmail.com>
2023-08-05 21:47:52 -0700

    Refactor, add lisp escape syntax (\`), make infix-boundaries and parens configurable

--- src/tokenizer.lisp
+++ src/tokenizer.lisp
@@ -37,88 +37,112 @@
     read))
 
 
+(defvar *parens* (cons "\\(" "\\)")
+  "Defines a pair of regexes for the open and close parens in an infix macro usage")
+(defvar *escape-open-prefix-regex* "`"
+  "Defines the regex to identify the start of a lisp section.
+Must be succeeded by the starting open-paren of the lisp code itself.")
+(defvar *escape-open-regex* "."
+  "Defines the bounds for the actual start-char of a lisp section")
+
 (defun tokenize (formula-str)
   (let* ((unsigned-value-regex "^[0-9]+(?:\\.[0-9]*)?(?:[dDeEfFlLsS][+-]?[0-9]+)?")
          (unsigned-double-quote-regex "^\".*?\"")
          (operator-regex-tmp (create-operator-regex))
          (operator-regex (format nil "^(~a)" operator-regex-tmp))
-         (separator-regex (format nil "~a|\\(|\\)|," operator-regex-tmp))
-         (unsigned-symbol-regex (format nil "^(.+?)(?:~a|\\s|\\z)" separator-regex)))  ; \z is required to terminate non-greedy match
+         (separator-regex (format nil (concatenate 'string "~a|"
+                                                   (car *parens*) "|"
+                                                   (cdr *parens*) "|,")
+                                  operator-regex-tmp))
+         (unsigned-symbol-regex (format nil "^(.+?)(?:~a|\\s|\\z)" separator-regex))) ; \z is required to terminate non-greedy match
     (let ((tokenized nil))
       (loop with rest-str = formula-str
             with sign-allowed = t
             if (zerop (length rest-str)) do (loop-finish)
-            do (acond
-                ((match-length "^\\s+" rest-str)
-                 (setf rest-str (subseq rest-str it))) ; remove spaces
+              do (acond
+                  ((match-length "^\\s+" rest-str)
+                   (setf rest-str (subseq rest-str it))) ; remove spaces
 
-                ((match-length "^#" rest-str) ;; parse reader macro
-                 (declare (ignore it))
-                 (multiple-value-bind (read-form position)
-                     (read-from-string rest-str)
-                   (push read-form tokenized)
-                   (setf rest-str (subseq rest-str position))
-                   (setf sign-allowed nil)))
+                  ((match-length "^#" rest-str) ;; parse reader macro
+                   (declare (ignore it))
+                   (multiple-value-bind (read-form position)
+                       (read-from-string rest-str)
+                     (push read-form tokenized)
+                     (setf rest-str (subseq rest-str position))
+                     (setf sign-allowed nil)))
 
-                ((and sign-allowed (match-length "^[+-]" rest-str))
-                 (declare (ignore it))
-                 (progn
-                   (when (char= (char rest-str 0) #\-)
+                  ((and sign-allowed (match-length "^[+-]" rest-str))
+                   (declare (ignore it))
+                   (progn
+                     (when (char= (char rest-str 0) #\-)
+                       (push *left-paren* tokenized)
+                       (push -1 tokenized)
+                       (push *right-paren* tokenized)
+                       (push (symbol-to-operator '*) tokenized))
+                     (setf rest-str (subseq rest-str 1))
+                     (setf sign-allowed nil)))
+
+                  ((match-length unsigned-value-regex rest-str)
+                   (progn
+                     (push (read-safely (subseq rest-str 0 it)) tokenized)
+                     (setf rest-str (subseq rest-str it))
+                     (setf sign-allowed nil)))
+
+                  ((match-length unsigned-double-quote-regex rest-str)
+                   (progn
+                     (push (read-safely (subseq rest-str 1 (- it 1))) tokenized) ; remove double quotes
+                     (setf rest-str (subseq rest-str it))
+                     (setf sign-allowed nil)))
+
+                  ((cl-ppcre:scan "^\"" rest-str)
+                   ;; Reaching here through the above conditions means unmatched double quote existing
+                   (declare (ignore it))
+                   (error "Unmatched double quote"))
+
+                  ((match-length operator-regex rest-str 0)
+                   (progn
+                     (push (symbol-to-operator (read-from-string (subseq rest-str 0 it))) tokenized)
+                     (setf rest-str (subseq rest-str it))
+                     (setf sign-allowed nil)))
+
+                  ((match-length (concatenate 'string "^" (car *parens*)) rest-str)
+                   (progn
+                   (print "open-paren")
                      (push *left-paren* tokenized)
-                     (push -1 tokenized)
+                     (setf rest-str (subseq rest-str it))
+                     (setf sign-allowed t)))
+
+                  ((match-length (concatenate 'string "^" (cdr *parens*)) rest-str)
+                   (progn
+                   (print "close-paren")
                      (push *right-paren* tokenized)
-                     (push (symbol-to-operator '*) tokenized))
-                   (setf rest-str (subseq rest-str 1))
-                   (setf sign-allowed nil)))
+                     (setf rest-str (subseq rest-str it))
+                     (setf sign-allowed nil)))
 
-                ((match-length unsigned-value-regex rest-str)
-                 (progn
-                   (push (read-safely (subseq rest-str 0 it)) tokenized)
-                   (setf rest-str (subseq rest-str it))
-                   (setf sign-allowed nil)))
+                  ((match-length "^," rest-str)
+                   (progn
+                     (push *separator* tokenized)
+                     (setf rest-str (subseq rest-str it))
+                     (setf sign-allowed t)))
 
-                ((match-length unsigned-double-quote-regex rest-str)
-                 (progn
-                   (push (read-safely (subseq rest-str 1 (- it 1))) tokenized) ; remove double quotes
-                   (setf rest-str (subseq rest-str it))
-                   (setf sign-allowed nil)))
+                  ((match-length (concatenate 'string "^" *escape-open-prefix-regex* *escape-open-regex*) rest-str)
+                   (declare (ignore it))
+                   (progn
+                     (setf rest-str (subseq rest-str (max 0 (length *escape-open-prefix-regex*))))
+                     (unless (match-length *escape-open-regex* rest-str)
+                       (error "Invalid lisp escape syntax in infix section: should start with \\("))
+                     (multiple-value-bind (contents len) (read-from-string rest-str)
+                       (push contents tokenized)
+                       (setf rest-str (subseq rest-str len)))
+                     (setf sign-allowed t)))
 
-                ((cl-ppcre:scan "^\"" rest-str)
-                 ;; Reaching here through the above conditions means unmatched double quote existing
-                 (declare (ignore it))
-                 (error "Unmatched double quote"))
+                  ((match-length unsigned-symbol-regex rest-str 0)
+                   (progn
+                     (push (read-safely (subseq rest-str 0 it)) tokenized)
+                     (setf rest-str (subseq rest-str it))
+                     (setf sign-allowed nil)))
 
-                ((match-length operator-regex rest-str 0)
-                 (progn
-                   (push (symbol-to-operator (read-from-string (subseq rest-str 0 it))) tokenized)
-                   (setf rest-str (subseq rest-str it))
-                   (setf sign-allowed nil)))
-
-                ((match-length "^\\(" rest-str)
-                 (progn
-                   (push *left-paren* tokenized)
-                   (setf rest-str (subseq rest-str it))
-                   (setf sign-allowed t)))
-
-                ((match-length "^\\)" rest-str)
-                 (progn
-                   (push *right-paren* tokenized)
-                   (setf rest-str (subseq rest-str it))
-                   (setf sign-allowed nil)))
-
-                ((match-length "^," rest-str)
-                 (progn
-                   (push *separator* tokenized)
-                   (setf rest-str (subseq rest-str it))
-                   (setf sign-allowed t)))
-
-                ((match-length unsigned-symbol-regex rest-str 0)
-                 (progn
-                   (push (read-safely (subseq rest-str 0 it)) tokenized)
-                   (setf rest-str (subseq rest-str it))
-                   (setf sign-allowed nil)))
-
-                (t
-                 (declare (ignore it))
-                 (error "Invalid sytax"))))
+                  (t
+                   (declare (ignore it))
+                   (error "Invalid sytax"))))
       (nreverse tokenized))))
